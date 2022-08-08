f5bf4b94cce63757d258c01e396efe16309ab9a5
Thomas Musselman <tmusselman@gmail.com>
2022-08-08 09:08:59 -0600

    test: item setup and attributes

---
+++ spec/item_spec.rb
@@ -0,0 +1,13 @@
+require './lib/item'
+
+RSpec.describe Item do
+  let(:item1) { Item.new({name: 'Peach', price: "$0.75"}) }
+  let(:item2) { Item.new({name: 'Tomato', price: '$0.50'}) }
+
+  it 'exists and has attributes' do
+    expect(item1).to be_instance_of(Item)
+    expect(item2.name).to eq("Tomato")
+    expect(item2.price).to eq(0.5)
+  end
+  
+end
