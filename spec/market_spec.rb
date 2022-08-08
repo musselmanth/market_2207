eb3079aa1cd19bafa33e35e8c543c0031e3b4da1
Thomas Musselman <tmusselman@gmail.com>
2022-08-08 10:31:13 -0600

    test: Market#overstocked_items

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -80,6 +80,10 @@ RSpec.describe Market do
       expect(market.sorted_item_list).to eq(["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"])
     end
 
+    it 'can return a list of overstocked items' do
+      expect(market.overstocked_items).to eq([item1])
+    end
+
   end
 
 end
