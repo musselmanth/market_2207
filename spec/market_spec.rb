611d6e8d4188892794c6bbcbf57c493341087e4d
Thomas Musselman <tmusselman@gmail.com>
2022-08-08 09:27:56 -0600

    test: market initialize and attributes

---
+++ spec/market_spec.rb
@@ -0,0 +1,28 @@
+require './lib/vendor'
+require './lib/market'
+
+RSpec.describe Market do
+  let(:market) { Market.new("South Pearl Street Farmers Market") }
+  let(:item1) { Item.new({name: 'Peach', price: "$0.75"}) }
+  let(:item2) { Item.new({name: 'Tomato', price: '$0.50'}) }
+  let(:item3) { Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"}) }
+  let(:item4) { Item.new({name: "Banana Nice Cream", price: "$4.25"}) }
+  let(:vendor) { Vendor.new("Rocky Mountain Fresh") }
+  let(:vendor2) { Vendor.new("Ba-Nom-a-Nom") }
+  let(:vendor3) { Vendor.new("Palisade Peach Shack") }
+
+  before(:each) do
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3.stock(item1, 65)
+  end
+
+  it 'exists and has attributes' do
+    expect(market).to be_instance_of(Market)
+    expect(market.name).to eq("South Pearl Street Farmers Market")
+    expect(market.vendors).to eq([])
+  end
+  
+end
