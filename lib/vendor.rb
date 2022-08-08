ed432be242fb3f5ad326a5a21ac7e94d13f50678
Thomas Musselman <tmusselman@gmail.com>
2022-08-08 09:21:58 -0600

    feat: Vendor class

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,2 +1,18 @@
 class Vendor
+
+  attr_reader :name, :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def stock(item, qty)
+    @inventory[item] += qty
+  end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+  
 end
